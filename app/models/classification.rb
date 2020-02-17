6816e4093f5bb99d4170b0f58d87fab6d86ecc3a
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-17 10:30:03 -0500

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,12 +2,14 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
+  # returns all classifications
   def self.my_all
-    # all
+    all
   end
 
+  # returns the classifications for the longest boat
   def self.longest
-    # Boat.longest.classifications
+    Boat.longest.classifications
   end
 
 end
